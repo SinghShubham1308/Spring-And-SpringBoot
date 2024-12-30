faeebeb96130be1f97daeb90a6a3d076dec8fb7e
SinghShubham1308 <shubhusingh1308@gmail.com>
2024-12-30 18:05:49 +0530

    created new project

---
+++ learn-Spring-framework01/src/main/java/com/spring/learn_Spring_framework01/LearnSpringFramework01Application.java
@@ -0,0 +1,13 @@
+package com.spring.learn_Spring_framework01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringFramework01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringFramework01Application.class, args);
+	}
+
+}
---
+++ learn-Spring-framework01/src/test/java/com/spring/learn_Spring_framework01/LearnSpringFramework01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.learn_Spring_framework01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringFramework01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
