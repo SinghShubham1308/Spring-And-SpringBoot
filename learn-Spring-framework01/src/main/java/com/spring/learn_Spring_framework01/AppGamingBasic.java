abce9060c9b88f58b295f42ff17aa6c9e3f25c99
SinghShubham1308 <shubhusingh1308@gmail.com>
2024-12-31 12:11:35 +0530

    loosely coupled

---
+++ learn-Spring-framework01/src/main/java/com/spring/learn_Spring_framework01/AppGamingBasic.java
@@ -0,0 +1,15 @@
+package com.spring.learn_Spring_framework01;
+
+import com.spring.learn_Spring_framework01.game.GameRunner;
+import com.spring.learn_Spring_framework01.game.PacmanGame;
+
+public class AppGamingBasic {
+	public static void main(String[] args) {
+//		MarioGame game = new MarioGame();
+//		ContraGame game = new ContraGame();
+		PacmanGame game = new PacmanGame();
+		GameRunner gameRunner = new GameRunner(game);
+
+		gameRunner.run();
+	}
+}
---
+++ learn-Spring-framework01/src/main/java/com/spring/learn_Spring_framework01/game/GameRunner.java
@@ -0,0 +1,31 @@
+package com.spring.learn_Spring_framework01.game;
+
+public class GameRunner {
+//	private MarioGame game;
+//	private ContraGame game;
+	private GamingConsole game;
+
+	/*
+	 * public GameRunner(MarioGame game) { super(); this.game = game; }
+	 */
+	
+	/*
+	 * public GameRunner(ContraGame game) { super(); this.game = game; }
+	 */
+	
+	public GameRunner(GamingConsole game) {
+		super();
+		this.game = game;
+	}
+
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("game is running"+game);
+		game.down();
+		game.up();
+		game.right();
+		game.left();
+		
+	}
+	
+}
