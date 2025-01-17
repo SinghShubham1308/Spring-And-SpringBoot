bde237f95d377b0c834887da58888a0728eb7ba5
SinghShubham1308 <shubhusingh1308@gmail.com>
2025-01-17 14:01:37 +0530

    Learning oAuth

---
+++ learn-oAuth/src/test/java/com/springboot/learn_oAuth/LearnOAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.learn_oAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnOAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
