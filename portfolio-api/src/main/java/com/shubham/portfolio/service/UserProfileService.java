3262c101ae4ad7d5959f596fa0895b6405264a56
SinghShubham1308 <shubhusingh1308@gmail.com>
2025-11-21 11:00:52 +0530

    handled null check and added logs

--- portfolio-api/src/main/java/com/shubham/portfolio/service/UserProfileService.java
+++ portfolio-api/src/main/java/com/shubham/portfolio/service/UserProfileService.java
@@ -3,10 +3,10 @@ package com.shubham.portfolio.service;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.hibernate.Hibernate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -65,11 +65,19 @@ public class UserProfileService {
 		LOGGER.debug("[UserProfileService][insertUpdatePortfolioData]  user profile is {}", profile);
 		profile.setUser(adminUser);
 
-		profile.setFeatures(Optional.ofNullable(profile.getFeatures()).orElseGet(HashSet::new));
-		profile.setSkills(Optional.ofNullable(profile.getSkills()).orElseGet(HashSet::new));
+
+		Hibernate.initialize(profile.getFeatures());
+		Hibernate.initialize(profile.getSkills());
+
+		// Ab safe null check
+		if (profile.getFeatures() == null) {
+		    profile.setFeatures(new HashSet<>());
+		}
+		if (profile.getSkills() == null) {
+		    profile.setSkills(new HashSet<>());
+		}
 
 		profile.getFeatures().clear();
-		profile.getSkills().clear();
 		profile.getSkills().clear();
 		LOGGER.debug("[UserProfileService][insertUpdatePortfolioData] setting PersonalDto from portfolioDataDto ");
 		PersonalDto personal = dataDto.getPersonal();
